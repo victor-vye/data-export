a5d38d6a54a93910c8cc6d7efdc39555696cebdf
Victor Ye <victor.ye@sas.com>
2023-04-03 09:26:22 +0200

    Added manifest folder
    Added manifest folder

---
+++ manifest/quick.deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Apply ddc_manifest.yaml to create the needed Deployment and Service
+
+kubectl -n big apply -f https://raw.githubusercontent.com/victor-vye/data-export/master/manifest/ddc_manifest.yaml
+
+# Create a manifest to define the Ingress resource
+cat << EOF > /tmp/ddc-ingress.yaml
+---
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: ddc-ingress
+  annotations:
+    kubernetes.io/ingress.class: nginx
+spec:
+  rules:
+    - host: big.$(hostname -f)
+      http:
+        paths:
+          - backend:
+              serviceName: ddc-service
+              servicePort: 3000
+            path: /ddc
+EOF
+
+# Apply the newly yaml file
+kubectl apply -f /tmp/ddc-ingress.yaml -n big
+
+# Print the URL of the web application
+printf "URL for DDC ingress: http://big.$(hostname -f)/ddc \n"
